2aecafb445f340917198868b6bc8434f195bebe3
Bernardo Camarate <b.m.m.c@hotmail.com>
2024-09-25 15:11:37 +0100

    removed unnecessary controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   root 'homepage#index'
   get '/*path' => 'homepage#index'
-  get 'signup', to: 'signup#index'  
+  get 'signup', to: 'users#new'  
 
   # signup routes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
